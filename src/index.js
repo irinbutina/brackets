90bd38afa023a6c46ed013b57221c4edf21a9795
irinbutina <1irinbutina@gmail.com>
2021-03-15 20:18:41 +0700

    docs: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    
+    console.log(str)
+    console.log(bracketsConfig)
+    while(str !== '') {    
+        let flag = false;
+        bracketsConfig.forEach(element => { 
+          let brackets = element[0] + element[1]; 
+          console.log(brackets)
+            if (str.includes(brackets)){ 
+                str = str.replace(brackets, ''); 
+                flag = true;
+            }
+        });
+        if (flag === false) break;
+      }
+      return (str === '') ? true  : false;
 }

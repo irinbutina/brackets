b0039599f68c8d02587bf5311dfa5c7acb4000d2
irinbutina <1irinbutina@gmail.com>
2022-07-29 18:44:43 +0700

    feat: solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
+module.exports = function check(str, bracketsConfig) { 
+   res = bracketsConfig.map(el => el.join('')) 
+   for (let i = 0; i < res.length; i++) {
+    if (str.includes(res[i])) {
+      str= str.replace(res[i], '');
+      i = -1;
+    }
+   }
+   return str.length === 0;
 }
